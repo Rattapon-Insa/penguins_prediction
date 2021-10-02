193c90d7c7afa79dcd091258847feb5775c6a414
rattaponcoding <66587676+rattaponcoding@users.noreply.github.com>
2021-10-02 15:37:58 +0700

    Add steamlit app, example data and pickle file

---
+++ main.py
@@ -0,0 +1,78 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+from sklearn.ensemble import RandomForestClassifier
+from tensorflow.python.util.tf_export import InvalidSymbolNameError
+
+st.write("""
+# Penguins Prediction app
+
+This app predict the **Palmer Penguin** species!
+
+Referenece Data from Data professor and palmerpenguins. 
+  
+""")
+
+st.sidebar.header('User input feature')
+st.sidebar.markdown("""
+
+[Example CSV file] (https://raw.githubusercontent.com/dataprofessor/data/master/penguins_example.csv)
+
+""")
+uploaded_file = st.sidebar.file_uploader('Upload your data here',type= ['csv'])
+
+if uploaded_file is not None:
+    input_df = pd.read_csv(uploaded_file)
+else:
+    def user_input_features():
+        island = st.sidebar.selectbox('Island',('Biscoe', 'Dream', "Torgersen"))
+        sex = st.sidebar.selectbox('Sex',('male', 'female'))
+        bill_length_mm = st.sidebar.slider('Bill length (mm)', 32,60,44)
+        bill_depth_mm = st.sidebar.slider('Bill depth (mm)', 13,21,17)
+        flipper_length_mm = st.sidebar.slider('Flipper length (mm)', 170, 230, 200)
+        body_mass_g = st.sidebar.slider(' Body Mass (g)', 2700, 6300, 4200)
+        data = {
+            'island':island,
+            'bill_length_mm' : bill_length_mm,
+            'bill_depth_mm':bill_depth_mm,
+            'flipper_length_mm': flipper_length_mm,
+            'body_mass_g': body_mass_g,
+            'sex' : sex
+            }
+        features = pd.DataFrame(data, index=[0])
+        return features
+    input_df = user_input_features()
+
+penguins_raw = pd.read_csv('penguins_cleaned.csv')
+penguins = penguins_raw.drop(columns= ['species'])
+df = pd.concat([input_df, penguins], axis = 0)
+
+encode = ['sex', 'island']
+
+for col in encode:
+    dummy = pd.get_dummies(df[col], prefix= col)
+    df = pd.concat([df,dummy], axis = 1)
+    del df[col]
+
+df = df[:1]
+
+st.subheader('User Input features')
+
+if uploaded_file is not None:
+    st.write(df)
+else:
+    st.write(' Awaiting CSV file to be upload. Currently using example input parameters.')
+    st.write(df)
+
+
+load_clf = pickle.load(open('model.pkl','rb'))
+prediction = load_clf.predict(df)
+prediction_prob = load_clf.predict_proba(df)
+
+st.subheader('Prediction')
+species = np.array(['Adelie', 'Chinstrap', "Gentoo"])
+st.write(species[prediction])
+
+st.subheader('Prediction Probability')
+st.write(prediction_prob)
